12459d484118ac607c9560f89a4cbb9dc2639cf3
priyanka234593 <priyanka.k@shaeryldatatech.com>
2025-08-22 10:25:50 +0530

    Update _app.tsx
    global routing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,9 @@ import React, { Fragment } from "react";
 import { wrapper } from "../store";
 import * as gtag from "../utils/gtag";
 
+// ✅ Import your traffic report component
+import TrafficReport from "../components/trafficreport/report";
+
 const isProduction = process.env.NODE_ENV === "production";
 
 // only events on production
@@ -29,11 +32,17 @@ const poppins = Poppins({
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Fragment>
+    {/* global font variable */}
     <style jsx global>{`
       :root {
         --main-font: ${poppins.style.fontFamily};
       }
     `}</style>
+
+    {/* ✅ Traffic report script injected globally */}
+    <TrafficReport />
+
+    {/* Render actual app page */}
     <Component {...pageProps} />
   </Fragment>
 );
